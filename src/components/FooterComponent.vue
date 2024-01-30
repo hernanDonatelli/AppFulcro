4cd78f89cf3e626dd9e7d0092b0746289281e74e
Hernan Donatelli <hernandonatelli@gmail.com>
2024-01-30 10:44:17 -0300

    agregado de logos de marcas, mejora en estilos y responsive

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -13,35 +13,37 @@ onMounted(() => {
 
 <template>
   <section id="footer" class="q-px-md q-pt-xl q-pb-md">
-    <div class="row justify-center main-container">
-      <div class="col-12">
-        <div class="row items-center justify-between">
-          <div class="mail flex justify-center items-center col-12 col-md-4">
-            <q-icon name="las la-envelope" />
-            <p class="text-center q-mb-none q-pl-sm">web@fulcro.com.ar</p>
-          </div>
+    <div class="wrapper">
+      <div class="row justify-center main-container">
+        <div class="col-12">
+          <div class="row items-center justify-between">
+            <div class="mail flex justify-center items-center col-12 col-md-4">
+              <q-icon name="las la-envelope" />
+              <p class="text-center q-mb-none q-pl-sm">web@fulcro.com.ar</p>
+            </div>
 
-          <div class="links text-center col-12 col-md-2">
-            <a class="q-px-md" href="https://ar.linkedin.com/company/fulcro-foods-retails" target="_blanck">
-              <q-icon name="lab la-linkedin" />
-              <q-tooltip self="center middle" style="color: #e8e8e8; background-color: transparent;"
-                transition-show="scale" transition-hide="scale">
-                Linkedin
-              </q-tooltip>
-            </a>
-          </div>
+            <div class="links text-center col-12 col-md-2">
+              <a class="q-px-md" href="https://ar.linkedin.com/company/fulcro-foods-retails" target="_blanck">
+                <q-icon name="lab la-linkedin" />
+                <q-tooltip anchor="top middle" self="center middle" style="color: #e8e8e8; background-color: transparent;"
+                  transition-show="scale" transition-hide="scale">
+                  Linkedin
+                </q-tooltip>
+              </a>
+            </div>
 
-          <div class="phone flex justify-center items-center text-center col-12 col-md-4">
-            <q-icon name="las la-phone" />
-            <p class="q-mb-none">11 5109-0509</p>
+            <div class="phone flex justify-center items-center text-center col-12 col-md-4">
+              <q-icon name="las la-phone" />
+              <p class="q-mb-none">11 5109-0509</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div class="copy row justify-center q-mt-lg">
-      <p class="text-center q-mb-none">Fulcro Foods & Retails © Copyright {{ actualYear }}.</p>
-      <p class="text-center q-mb-none">Todos los derechos reservados</p>
+      <div class="copy row justify-center q-mt-lg">
+        <p class="text-center q-mb-none">Fulcro Foods & Retails © Copyright {{ actualYear }}.</p>
+        <p class="text-center q-mb-none">Todos los derechos reservados</p>
+      </div>
     </div>
   </section>
 </template>
@@ -51,11 +53,11 @@ onMounted(() => {
 #footer {
   background-color: #0a0c29;
   font-family: $montserrat;
-  font-size: clamp(.9rem, 1.3vw, 2rem);
+  font-size: clamp(.9rem, 1.1vw, 2rem);
   color: #82b1ff;
 
   .copy {
-    font-size: clamp(.8rem, 1.2vw, 1.2rem);
+    font-size: clamp(.8rem, 1vw, 1.2rem);
   }
   .mail {
     order: 2;
@@ -106,6 +108,16 @@ onMounted(() => {
 
     .phone {
       order: 3;
+    }
+  }
+}
+
+@media screen and (min-width: 1279.98px) {
+  #footer {
+
+    .wrapper{
+      max-width: 1100px;
+      margin: 0 auto;
     }
   }
 }
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -7,62 +7,47 @@ const imagenes = ref([
   {
     marca: 'Masseube',
     src: 'src/assets/marcas/masseube.jpg',
-    tiempo: 100
+    tiempo: 250
   },
   {
     marca: 'Chocolate Colonial',
     src: 'src/assets/marcas/colonial.jpg',
-    tiempo: 400
-  },
-  {
-    marca: 'Pariggi',
-    src: 'src/assets/marcas/pariggi.jpg',
-    tiempo: 700
-  },
-  {
-    marca: 'Pollo Cocido',
-    src: 'src/assets/marcas/pollo-cocido.jpg',
-    tiempo: 1000
+    tiempo: 500
   },
   {
     marca: 'Inti Zen',
     src: 'src/assets/marcas/inti-zen.jpg',
-    tiempo: 1300
+    tiempo: 750
   },
   {
-    marca: 'Gauchada',
-    src: 'src/assets/marcas/gauchada.jpg',
-    tiempo: 1600
+    marca: 'Molé',
+    src: 'src/assets/marcas/mole.jpg',
+    tiempo: 1000
   },
   {
-    marca: 'Sendero Sur',
-    src: 'src/assets/marcas/senderoSur.jpg',
-    tiempo: 1900
+    marca: 'Samkya',
+    src: 'src/assets/marcas/samkya.jpg',
+    tiempo: 1250
   },
   {
-    marca: '27',
-    src: 'src/assets/marcas/27.jpg',
-    tiempo: 2200
+    marca: 'Pirenco Patagonia',
+    src: 'src/assets/marcas/pirenco.jpeg',
+    tiempo: 1500
   },
   {
-    marca: 'Michelob',
-    src: 'src/assets/marcas/michelob.jpg',
-    tiempo: 2500
+    marca: 'Pollo Cocido',
+    src: 'src/assets/marcas/pollo-cocido.jpg',
+    tiempo: 1750
   },
   {
-    marca: 'Inti Zen',
-    src: 'src/assets/marcas/verde-negro.jpg',
-    tiempo: 2800
-  },
-  {
-    marca: 'Inti Hebras',
-    src: 'src/assets/marcas/inti-hebras.jpg',
-    tiempo: 3100
+    marca: 'Pariggi',
+    src: 'src/assets/marcas/pariggi.jpg',
+    tiempo: 2000
   },
   {
     marca: 'Smoke Cave',
     src: 'src/assets/marcas/smoke.jpg',
-    tiempo: 3400
+    tiempo: 2250
   }
 ])
 </script>
@@ -71,18 +56,21 @@ const imagenes = ref([
   <q-page>
     <!-- Intro -->
     <div id="intro" class="flex column items-center justify-center">
-      <img alt="Fulcro" src="../assets/logo-fulcro-home.jpeg" style="width: 400px; height: 400px">
-      <p class="text-center">Dame un punto de apoyo y moveré el mundo.</p>
-      <span class="text-italic">(Arquímedes 287-212 AC)</span>
+      <img data-aos="zoom-in" data-aos-duration="1000" data-aos-easing="ease" alt="Fulcro" src="../assets/logo-fulcro-home.jpeg" style="width: 350px; height: 350px">
+
+      <div class="flex column items-center q-mt-md">
+        <p class="text-center q-mb-none">"Dame un punto de apoyo y moveré el mundo"</p>
+        <span class="text-italic">(Arquímedes 287-212 AC)</span>
+      </div>
     </div>
 
     <!-- Marcas  -->
     <div id="marcas">
       <div class="row container q-mb-xl">
         <div class="col-auto q-mx-auto">
-          <div class="flex justify-center items-center q-gutter-md">
-            <div v-for="imagen in imagenes" :key="imagen.marca" class="col-3">
-              <img data-aos="zoom-in" :data-aos-duration="imagen.tiempo" data-offset="200" :src="imagen.src" :alt="imagen.marca" />
+          <div class="flex justify-center items-center q-gutter-md q-col-gutter-x-lg q-col-gutter-y-sm">
+            <div v-for="imagen in imagenes" :key="imagen.marca" class="col-3 col-md-4">
+              <img data-aos="zoom-in" data-aos-once="true" data-aos-easing="ease-in-sine" :data-aos-duration="500" data-offset="400" :data-aos-delay="imagen.tiempo" :src="imagen.src" :alt="imagen.marca" />
             </div>
           </div>
         </div>
@@ -97,17 +85,26 @@ const imagenes = ref([
 #intro {
   height: 100vh;
   font-family: $montserrat;
-  color: $blue-10;
+
+  p{
+    font-size: clamp(1rem, 1vw, 2rem);
+  }
 
 }
 
 #marcas {
+  height: auto;
   max-width: 992px;
   margin: 0 auto;
+
+  img{
+    max-width: 300px;
+  }
 }
 
 @media screen and (min-width: 1023.98px) {
   #marcas {
+    max-width: 992px;
 
     img{
       max-width: 250px;
@@ -117,17 +114,28 @@ const imagenes = ref([
 
 @media screen and (min-width: 1279.98px) {
   #marcas {
-    max-width: 1200px;
+    max-width: 960px;
 
     img{
-      max-width: 275px;
+      max-width: 230px;
     }
   }
 }
 
 @media screen and (min-width: 1365.98px) {
   #marcas {
-    max-width: 1280px;
+
+
+    img{
+      max-width: 250px;
+    }
+  }
+}
+
+@media screen and (min-width: 1599.98px) {
+  #marcas {
+    max-width: 1100px;
+
   }
 }
 </style>
